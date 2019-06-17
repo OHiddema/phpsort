f92fa2bbda67a1c5bcdd256bbb68d1de745b4277
Oege Hiddema <oegehiddema@gmail.com>
2019-06-17 11:19:11 +0200

    define basic workflow

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+//read file into an array, each element corresponds to a line
+$read = file('input.txt');
+
+// 1. Put the lines in an associative array
+// 2. Sort the array on the values
+// 3. Put the sorted array in a new file called 'output.txt'
