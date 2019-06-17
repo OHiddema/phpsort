abaf681a7908912c288c43137580f1f2cce23425
Oege Hiddema <oegehiddema@gmail.com>
2019-06-17 12:40:34 +0200

    sort ascending instead of descending

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ foreach ($read as $line) {
  }
 
 // 2. Sort the array on the values
-arsort($elem);
+asort($elem);
 
 // 3. Put the sorted array in a new file called 'output.txt'
 $myfile = fopen("output.txt", "w");
