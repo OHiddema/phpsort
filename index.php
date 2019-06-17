fe6b9fa38924673857e34db8d96b483deb80ca2c
Oege Hiddema <oegehiddema@gmail.com>
2019-06-17 11:31:54 +0200

    put the lines in  an associative array

--- index.php
+++ index.php
@@ -3,5 +3,12 @@
 $read = file('input.txt');
 
 // 1. Put the lines in an associative array
+foreach ($read as $line) {
+   $pos = strpos($line, ',');
+   $key = substr($line, 0, $pos);
+   $value = substr($line, $pos+1);
+   $elem[$key] = $value;
+ }
+
 // 2. Sort the array on the values
 // 3. Put the sorted array in a new file called 'output.txt'
